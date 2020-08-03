8f768557325b324da718891c3e4c93344e1d4a84
Daniel Gomez Aguirre <lvtcrp@gmail.com>
2020-08-03 15:52:43 -0500

    se da solucion a la descerealizacion de contenido con cookies haciendo cambios en la pk de las tablas root y admin a cadenas de texto para identificar el tipo en efecto

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -11,15 +11,15 @@ passport.use('local.signup.root', new LocalStrategy({
     passwordField: 'password',
     passReqToCallback: true
 }, async(req, username, password, done) => {
-
     const newUser = {
+        idUsuario_root: fk_root(),
         nombre_usuario_root: username,
         contrasena_usuario_root: password,
         tipo_usuario_root: 'root'
     };
     newUser.contrasena_usuario_root = await helpers.encryptPassword(password);
     const result = await pool.query('INSERT INTO usuario_root SET?', [newUser]);
-    newUser.idUsuario_root = result.insertId;
+    //newUser.idUsuario_root = result.insertId;
     return done(null, newUser);
     console.log(newUser)
 }));
@@ -39,9 +39,34 @@ passport.use('local.signin.root', new LocalStrategy({
             done(null, false, req.flash('message', 'Contraseña incorrecta'));
         }
     } else {
-        return done(null, false, req.flash('message', 'el usuario no se encuentra registrado'));
+        return done(null, false, req.flash('message', 'No hay una cuenta de acceso para el usuario'));
     }
 }));
+
+
+// Admin
+passport.use('local.signin.admin', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true
+}, async(req, username, password, done) => {
+    const rows = await pool.query('SELECT * FROM usuario_admin WHERE Nombre_Usuario_admin = ?', [username]);
+    if (rows.length > 0) {
+        const user = rows[0];
+        const validPassword = await helpers.mathPassword(password, user.Contrasena_Usuario_admin);
+        if (validPassword) {
+            done(null, user, req.flash('success', `Bienvenido ${user.Nombre_Usuario_admin.toUpperCase()}!!!`));
+        } else {
+            done(null, false, req.flash('message', 'Contraseña incorrecta'));
+        }
+    } else {
+        return done(null, false, req.flash('message', `El usuario ${username} no se encuentra registrado en el sistema`));
+    }
+}));
+
+// Usuario
+
+
 
 passport.serializeUser((user, done) => {
     if (user.tipo_usuario_root != undefined) {
@@ -57,16 +82,33 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser(async(id, done) => {
-    console.log('-------des-----------');
-    console.log(typeof(id))
-    console.log('------------------');
-    const rows = await pool.query('SELECT * FROM usuario_root WHERE idUsuario_root = ?', [id]);
-    done(null, rows[0]);
-})
+    if (id.length === undefined) {
 
-// Admin
+    } else if (id.split('')[0] === 'r') {
+        console.log('descerealizacion de un usuario root')
+        const rows = await pool.query('SELECT * FROM usuario_root WHERE idUsuario_root = ?', [id]);
+        done(null, rows[0]);
+    } else if (id.split('')[0] === 'a') {
 
+    }
+});
 
+let fk_root = () => {
+    let result = 'r-';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < 8; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+};
 
-
-// Usuario
+let fk_admin = () => {
+    let result = 'a-';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < 8; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+};
